d03c3945d3e75e09be7406b5fdde4bbda9cf5ac3
Tristan Rivoallan <tristan@rivoallan.net>
2024-12-13 08:17:39 +0100

    Correction des chemins d'accès pour le script de scraping et le script de génération HTML

--- src/build.py
+++ src/build.py
@@ -4,7 +4,7 @@ import random
 from jinja2 import Environment, FileSystemLoader
 
 # Charger les données JSON
-with open("../docs/results.json", "r", encoding="utf-8") as f:
+with open("./docs/results.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 # Sélectionner un texte, une image et un audio au hasard
@@ -13,14 +13,14 @@ image = random.choice(data["images"])
 audio = random.choice(data["audios"])
 
 # Configurer Jinja2 pour charger le modèle HTML
-env = Environment(loader=FileSystemLoader("."), autoescape=True)
+env = Environment(loader=FileSystemLoader("./src"), autoescape=True)
 template = env.get_template("template.html")
 
 # Rendre le modèle avec les données aléatoires
 html_content = template.render(text=text, image=image, audio=audio)
 
 # Enregistrer le contenu HTML dans un fichier
-output_file = "../docs/index.html"
+output_file = "./docs/index.html"
 with open(output_file, "w", encoding="utf-8") as f:
     f.write(html_content)
 
